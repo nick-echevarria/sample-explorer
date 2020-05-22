247d43f9171fa429216aaa0506761f0abc645b43
nick-echevarria <nick.echev@gmail.com>
2020-05-22 14:48:35 -0400

    Component Setup

---
+++ src/components/MainContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class MainContainer extends React.Component { 
+    state = { 
+        allSongs: []
+    }
+    //SearchBar Component
+    //Playist Component 
+
+}
+
+export default MainContainer;
